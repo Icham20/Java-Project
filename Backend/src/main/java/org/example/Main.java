823a53b8e4711188d55f4ff40ac0e33d1d16fdda
El0uan0 <elouan.quentel@gmail.com>
2026-01-05 13:27:54 +0100

    setup2

--- Backend/src/main/java/org/example/Main.java
+++ Backend/src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ import io.javalin.Javalin;
 public class Main {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7000);
-        app.get("/ping", ctx -> ctx.result("Le serveur répond !"));
+        app.get("/ping", ctx -> ctx.result("serveur !"));
     }
 }
